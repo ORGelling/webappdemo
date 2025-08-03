3f18861a736c2da43701268ff784db211a034958
ORGelling <o.r.gelling@hotmail.com>
2025-08-03 12:36:14 +0200

    Added lessons text file and some more steps from the tutorial

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Product;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $product = new Product;
+        $product->setName('Product One');
+        $product->setDescription('This is the first product.');
+        $product->setSize(100);
+        
+        $manager->persist($product);
+
+        $product = new Product;
+        $product->setName('Product Two');
+        $product->setDescription('This is the Second product.');
+        $product->setSize(200);
+
+        $manager->persist($product);
+
+        $product = new Product;
+        $product->setName('Product Three');
+        $product->setDescription('This is the third product.');
+        $product->setSize(300);
+
+        $manager->persist($product);
+
+        $manager->flush();
+    }
+}
